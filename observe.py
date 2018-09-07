9b7b24a7b3a61d68ff7ab8c2e40604ff660fcc98
fd17 <felixdietrich@gmail.com>
2018-09-07 14:24:30 +0200

    cleanup and more showcasing

--- observe.py
+++ observe.py
@@ -24,7 +24,7 @@ brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 
 # reset the environment
-env_info = env.reset(train_mode=True)[brain_name]
+env_info = env.reset(train_mode=False)[brain_name]
 
 # get action and state vector dimensions
 action_size = brain.vector_action_space_size
@@ -35,8 +35,6 @@ state_size = len(state)
 agent = Agent(state_size=state_size, action_size=action_size, seed=0)
 agent.qnetwork_local.load_state_dict(torch.load('checkpoints/checkpoint.pth'))
 
-env_info = env.reset(train_mode=False)[brain_name] # reset the environment
-state = env_info.vector_observations[0]            # get the current state
 score = 0                                          # initialize the score
 while True:
     action = int(agent.act(state, 0))              # select an action
